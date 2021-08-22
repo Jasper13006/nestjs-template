65a9562c6e558ec520b2faf9e2e6752e1c9dc080
Jasper13006 <jasper13006@gmail.com>
2021-08-22 09:42:05 -0500

    feat: add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load:[configuration],
+      cache:true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,18 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { createdocument } from './config/swagger/swaggerConfig';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // if (configService.get('ENVIRONMENT') === 'DEVELOPMENT') {
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('PORT');
+
+  if (configService.get('ENVIRONMENT') === 'DEVELOPMENT') {
     SwaggerModule.setup('docs', app, createdocument(app));
-  // }
-  await app.listen(3000);
+  }
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
